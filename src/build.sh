84a6a9af40e0ef52b18a5deade25a962f5351340
manu-spherical <manuhegdev@gmail.com>
2018-02-03 17:47:16 +0530

    setup with OpenCv

---
+++ src/build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/zsh
+cmake . && make -j4
--- src/fsort.cpp
+++ src/fsort.cpp
@@ -1,24 +1,24 @@
-#include<stdio.h>
-#include<opencv2/opencv.hpp>
+#include <GLFW/glfw3.h>
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
 
 using namespace cv;
 
-int main(int argc,char **argv)
-{
-  if(argc!=2)
-    {
-      printf("No args!");
-      return -1;
-    }
-  Mat image =imread(argv[1],1);
-  if(!image.data)
-    {
-      printf("No img data");
-      return -1;
-    }
-  namedWindow("Display Image",WINDOW_AUTOSIZE);
-  imshow("Display image",image);
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    printf("No args!");
+    return -1;
+  }
+  if (!glfwInit()) {
+    printf("Error!");
+  }
+  Mat image = imread(argv[1], 1);
+  if (!image.data) {
+    printf("No img data");
+    return -1;
+  }
+  //namedWindow("Display Image", WINDOW_AUTOSIZE);
+  imshow(argv[1], image);
   waitKey(0);
   return 0;
-    
 }
