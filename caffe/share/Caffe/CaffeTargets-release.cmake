380d52bb20f4836439b508105f9624c32fc0a9a7
manu-spherical <manuhegdev@gmail.com>
2018-04-18 23:33:27 +0530

    everything works fine! :D :D :D + missing  caffe folder included

---
+++ caffe/share/Caffe/CaffeTargets-release.cmake
@@ -0,0 +1,31 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "caffe" for configuration "Release"
+set_property(TARGET caffe APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(caffe PROPERTIES
+  IMPORTED_LINK_INTERFACE_LIBRARIES_RELEASE "caffeproto;/usr/lib/libboost_system.so;/usr/lib/libboost_thread.so;/usr/lib/libboost_filesystem.so;/usr/lib/libboost_chrono.so;/usr/lib/libboost_date_time.so;/usr/lib/libboost_atomic.so;/usr/local/lib/libglog.so;/usr/lib/libgflags.so;/usr/local/lib/libprotobuf.so;-lpthread;/usr/lib/libhdf5_cpp.so;/usr/lib/libhdf5.so;/usr/lib/librt.so;/usr/lib/libpthread.so;/usr/lib/libz.so;/usr/lib/libdl.so;/usr/lib/libm.so;/usr/lib/libhdf5_hl_cpp.so;/usr/lib/libhdf5_hl.so;/usr/lib/liblmdb.so;/usr/lib/libleveldb.so;opencv_core;opencv_highgui;opencv_imgproc;opencv_imgcodecs;/usr/lib/liblapack.so;/usr/lib/libptcblas.so;/usr/lib/libatlas.so;/usr/lib/libboost_python.so"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib64/libcaffe.so.1.0.0"
+  IMPORTED_SONAME_RELEASE "libcaffe.so.1.0.0"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS caffe )
+list(APPEND _IMPORT_CHECK_FILES_FOR_caffe "${_IMPORT_PREFIX}/lib64/libcaffe.so.1.0.0" )
+
+# Import target "caffeproto" for configuration "Release"
+set_property(TARGET caffeproto APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(caffeproto PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LINK_INTERFACE_LIBRARIES_RELEASE "/usr/local/lib/libprotobuf.so;-lpthread"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib64/libcaffeproto.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS caffeproto )
+list(APPEND _IMPORT_CHECK_FILES_FOR_caffeproto "${_IMPORT_PREFIX}/lib64/libcaffeproto.a" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
