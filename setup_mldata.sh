c704b7cd58d5816bd4ce6d045e1acca010c6f0cb
MohanKrishnaS <mohansuryanarayana@gmail.com>
2018-04-19 17:45:08 +0530

    flags added for laptop compatability

--- setup_mldata.sh
+++ setup_mldata.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir -p ml_data
 cd ml_data
-wget http://dl.caffe.berkeleyvision.org/bvlc_googlenet.caffemodel
-#mv bvlc_googlenet.caffemodel models/bvlc_googlenet/
+wget http://dl.caffe.berkeleyvision.org/bvlc_reference_caffenet.caffemodel
+mv bvlc_reference_caffenet.caffemodel models/bvlc_reference_caffenet/
  mkdir -p ilsvrc12
  cd ilsvrc12
  wget -c http://dl.caffe.berkeleyvision.org/caffe_ilsvrc12.tar.gz
