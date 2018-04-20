ec38081d61c2a075f2755136fdcb275c0336f3b7
gk1000 <manuhegdev@gmail.com>
2018-04-20 08:36:11 +0530

    mvs binaries

--- caffe/share/Caffe/CaffeTargets-release.cmake
+++ caffe/share/Caffe/CaffeTargets-release.cmake
@@ -8,7 +8,7 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 # Import target "caffe" for configuration "Release"
 set_property(TARGET caffe APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(caffe PROPERTIES
-  IMPORTED_LINK_INTERFACE_LIBRARIES_RELEASE "caffeproto;/usr/lib/libboost_system.so;/usr/lib/libboost_thread.so;/usr/lib/libboost_filesystem.so;/usr/lib/libboost_chrono.so;/usr/lib/libboost_date_time.so;/usr/lib/libboost_atomic.so;/usr/local/lib/libglog.so;/usr/lib/libgflags.so;/usr/local/lib/libprotobuf.so;-lpthread;/usr/lib/libhdf5_cpp.so;/usr/lib/libhdf5.so;/usr/lib/librt.so;/usr/lib/libpthread.so;/usr/lib/libz.so;/usr/lib/libdl.so;/usr/lib/libm.so;/usr/lib/libhdf5_hl_cpp.so;/usr/lib/libhdf5_hl.so;/usr/lib/liblmdb.so;/usr/lib/libleveldb.so;opencv_core;opencv_highgui;opencv_imgproc;opencv_imgcodecs;/usr/lib/liblapack.so;/usr/lib/libptcblas.so;/usr/lib/libatlas.so;/usr/lib/libboost_python.so"
+  IMPORTED_LINK_INTERFACE_LIBRARIES_RELEASE "caffeproto;/usr/lib/libboost_system.so;/usr/lib/libboost_thread.so;/usr/lib/libboost_filesystem.so;/usr/lib/libboost_chrono.so;/usr/lib/libboost_date_time.so;/usr/lib/libboost_atomic.so;/usr/lib/libglog.so;/usr/lib/libgflags.so;/usr/lib/libprotobuf.so;-lpthread;/usr/lib/libhdf5_cpp.so;/usr/lib/libhdf5.so;/usr/lib/libsz.so;/usr/lib/libz.so;/usr/lib/libdl.so;/usr/lib/libm.so;/usr/lib/libhdf5_hl_cpp.so;/usr/lib/libhdf5_hl.so;/usr/lib/liblmdb.so;/usr/lib/libleveldb.so;opencv_core;opencv_highgui;opencv_imgproc;opencv_imgcodecs;/usr/lib/liblapack.so;/usr/lib/libptcblas.so;/usr/lib/libatlas.so;/usr/lib/libboost_python.so"
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib64/libcaffe.so.1.0.0"
   IMPORTED_SONAME_RELEASE "libcaffe.so.1.0.0"
   )
@@ -20,7 +20,7 @@ list(APPEND _IMPORT_CHECK_FILES_FOR_caffe "${_IMPORT_PREFIX}/lib64/libcaffe.so.1
 set_property(TARGET caffeproto APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(caffeproto PROPERTIES
   IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
-  IMPORTED_LINK_INTERFACE_LIBRARIES_RELEASE "/usr/local/lib/libprotobuf.so;-lpthread"
+  IMPORTED_LINK_INTERFACE_LIBRARIES_RELEASE "/usr/lib/libprotobuf.so;-lpthread"
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib64/libcaffeproto.a"
   )
 
